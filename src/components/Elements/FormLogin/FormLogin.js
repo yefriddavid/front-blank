4fe3cce46213ab654abf001a286e3f252faa0979
Jeffrey David Rios <yefriddavid@gmail.com>
2018-05-28 13:10:47 -0300

    Update FormLogin.js

--- src/components/Elements/FormLogin/FormLogin.js
+++ src/components/Elements/FormLogin/FormLogin.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-//import MicroIcon from 'react-microvoz-icons';
+
 import { Grid, Row, Form, FormGroup, Col, Checkbox, ControlLabel, Button, FormControl, Panel } from 'react-bootstrap'
 
 import './FormLogin.css'
@@ -11,7 +11,6 @@ class FormLogin extends Component {
       username: null,
       password: null
     })
-    //this.onSubmit = this.onSubmit.bind(this)
   }
   onInputChange(e){
     this.setState({[e.target.id]: e.target.value})
@@ -32,16 +31,10 @@ class FormLogin extends Component {
           <img className="FormLogin__ProfileImage" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
               alt="" />
           <Form className="FormLogin" onSubmit={ this.onSubmit.bind(this) } >
-            {/*<input type="text" className="form-control FormLogin__Input FormLogin__Input--email" placeholder="Email" required autofocus />
-            {/*<input type="password" className="form-control FormLogin__Input FormLogin__Input--password" placeholder="Password" required />*/}
-  <FormControl className="FormLogin__Input FormLogin__Input--email" type="text" id="email" placeholder="Email" value={ username } onChange = { this.onInputChange.bind(this) } />
-  <FormControl className="FormLogin__Input FormLogin__Input--password" type="password" id="password" placeholder="Password" value={ username } onChange = { this.onInputChange.bind(this) } />
+            <FormControl className="FormLogin__Input FormLogin__Input--email" type="text" id="email" placeholder="Email" value={ username } onChange = { this.onInputChange.bind(this) } />
+            <FormControl className="FormLogin__Input FormLogin__Input--password" type="password" id="password" placeholder="Password" value={ username } onChange = { this.onInputChange.bind(this) } />
             <Button type="submit"  className="btn-lg btn-primary btn-block">Sign in</Button>
-            {/*}<label className="FormLogin__Rememberme checkbox pull-left">*/}
-              <Checkbox className='FormLogin__Rememberme__Input'>Remember me</Checkbox>
-              {/*}<input className='FormLogin__Rememberme__Input' type="checkbox" value="remember-me" />
-                Remember me*/}
-                {/*}</label>*/}
+            <Checkbox className='FormLogin__Rememberme__Input'>Remember me</Checkbox>
             <a href="#" className="pull-right FormLogin__NeedHelp">Need help? </a>
             <span class="clearfix"></span>
           </Form>
