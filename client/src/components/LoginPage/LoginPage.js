ae973461f7675e5377025cd297a40fd0bc89a3ce
Jeffrey Rios <yefriddavid@gmail.com>
2018-05-31 14:57:36 -0300

    m

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import FormLogin from "../Elements/FormLogin/FormLogin"
 
+import styles from './LoginPage.css'
 export default class LoginPage extends Component {
   render() {
+    console.log(styles)
     return (
-      <FormLogin {...this.props} />
+      <FormLogin className={styles} {...this.props} />
     )
   }
 }
